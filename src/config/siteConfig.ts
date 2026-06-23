f229177249ae1569b84a05df15bced82639298db
Liuli <3147098572@qq.com>
2026-06-23 17:12:22 +0800

    修改siteConfig配置

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,10 +6,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "个人博客",
+	title: "琉璃的博客",
 
 	// 站点副标题
-	subtitle: "one Blog",
+	subtitle: "Welcome to my blog！",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
